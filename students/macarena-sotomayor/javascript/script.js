a9644ea48b2186f1883f7c0b51382c6882644b26
macaeco <macarenafernandezayusob@gmail.com>
2022-01-19 18:07:47 +0100

    ejercicios dia 19 javascript

---
+++ students/macarena-sotomayor/javascript/script.js
@@ -0,0 +1,156 @@
+
+// prueba inicial
+
+// alert("hola mundo");
+// document.write("<h2>Hola mundo</h2>");
+
+// let nombre = prompt("escribe algo aqui");
+
+// document.write(nombre);
+
+// let numero = 20;
+
+// let verdad = true;
+
+// let nada;
+
+// let cer = null;
+
+// document.write(typeof numero)
+// document.write(typeof verdad)
+// document.write(typeof nada)
+// document.write(typeof cer)
+
+// apartado 1
+
+// let num1 = 15;
+// let num2 = 16;
+
+// document.write('<p>El resultado es: '+(num1 + num2)+' </p>' ); 
+
+
+
+//apartado2
+
+
+// let var1 = 'hola';
+// let var2 = 'adios';
+
+// document.write(var1 + var2);
+ 
+//apartado3
+
+// let cadena = 'var1+var2'; 
+// tambien vale sin comillas el val1+val2
+// document.write(cadena.length);
+// document.write((var1+var2).length);
+
+
+// apartado 4y5
+// document.write('   ');
+
+
+// let gradosc= prompt ('Introduce grados');
+
+// let farenheit= ((gradosc*(9/5))+32);
+// document.write(farenheit)
+
+
+// let f87 = 87;
+
+// if (farenheit > 87) {
+//     document.write('mayor');
+// }
+
+
+// if (farenheit < 87) {
+//     document.write('menor');
+// }
+
+
+// if (farenheit = 87) {
+//     document.write('igual');
+// }
+
+// et comparacion = 87-farenheit;l
+
+
+
+// document.write(farenheit > 87);
+
+//apartado 6
+
+// document.write( true && false);
+// document.write( true && true);
+
+// document.write(cifra1+cifra2);
+
+// let cifra1= "5";
+// let cifra2= "5";
+
+// document.write(cifra1+cifra2);
+
+
+// document.write(' ')
+
+
+// let  suma= 27.5;
+// let suma2= 27.5;
+
+
+
+// let resultadosuma=(suma+suma2);
+// document.write(resultadosuma);
+
+//apartado7
+
+// let numero1= 4;
+// let numero2= 5;
+// let verdadero= numero2 > numero1;
+// let falso= numero2 < numero1;
+
+
+// document.write(verdadero);
+
+
+
+// document.write(falso);
+
+// document.write(' ');
+
+// document.write(verdadero||falso);
+// document.write(falso&&falso);
+
+// document.write(' ');
+
+//apartado 8
+
+
+// let peso= prompt ("introducir peso")
+// let altura= prompt ("introducir altura")
+
+// let relacion= (peso/(altura*altura));
+
+// document.write( relacion < 35);
+
+
+//APARTADO 9
+
+let numerico= prompt('intoducir numero');
+
+let resto=numerico%2;
+let respuesta= 1>resto;
+
+document.write('<p> es par? </p>' + respuesta);
+
+
+
+
+
+
+
+
+
+
+
+
