402b316a038d64cb13bccea3a23756268fa9c6e6
DESKTOP-6JOKMHA\usuario <healfer@yahoo.es>
2022-01-24 18:17:50 +0100

    ejercicios funciones 24/01 tarde

---
+++ students/hector-alvarez/ejercicios/ejercicions funciones/alex/funciones.js
@@ -0,0 +1,21 @@
+/*
+
+1. Crear una función que devuelva la resta de dos números. Llamad a la función 3 veces con valores distintos y pintar los resultados en un p cada uno
+
+*/
+
+
+/*
+// Con funciones tradicionales con nombre
+function substract(a,b){
+    return a-b;
+ }
+
+ // con arrow function
+ const substract = (a,b) => a-b;
+ 
+ document.write(`<p>${substract(3,4)}</p>`);
+ document.write(`<p>${substract(6,1)}</p>`);
+ document.write(`<p>${substract(88888,88888)}</p>`);
+ 
+ */
