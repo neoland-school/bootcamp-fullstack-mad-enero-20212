c2c9619ed7953b3a9bc68190c6a105428f99ffaa
Robinbucad <robin.bucad6@gmail.com>
2022-02-08 14:26:58 +0100

    aplicando las mejoras a la bateria

--- students/robin/promesas-GOOGLEMEETS-BATTERY-7-02/battery-exercise/script.js
+++ students/robin/promesas-GOOGLEMEETS-BATTERY-7-02/battery-exercise/script.js
@@ -6,21 +6,34 @@ function showBattery(){
     const pBatt = document.createElement('p')
     batt.appendChild(pBatt)
 
-
-    console.log(batteryIsCharging)
     batteryIsCharging = battery.charging;
-    console.log(batteryIsCharging)
-    batteryLevel = batteryIsCharging*100
-    console.log(batteryLevel)
+    
+     batteryLevel = battery.level*100
     changeDivColor(battery)
     pBatt.textContent = `${batteryLevel}%`
 
-    
+  
     if(batteryIsCharging === true){
         const pInfo = document.createElement('p')
         pInfo.textContent = "Battery Charging"
         batt.appendChild(pInfo)
+    }else if(batteryIsCharging === false ){
+        const pInfo = document.createElement('p')
+        pInfo.textContent = 'Not charging'
+        batt.appendChild(pInfo)
+        batt.appendChild(pInfo)
     }
+
+ 
+
+    if(batteryLevel < 50 && batteryLevel > 20){
+        const batteryColor = document.querySelector('.battery');
+        batteryColor.style.backgroundColor = 'orange'
+    }else if (batteryLevel < 20){
+        const batteryColor = document.querySelector('.battery');
+        batteryColor.style.backgroundColor = 'red'
+    }
+    
 })
 }
 showBattery()
@@ -54,15 +67,55 @@ function changeDivColor(){
                const div2 = document.querySelectorAll('.indicator');
                div2.forEach(value => value.remove('indicator'))
                counter = 0
-            }
+              
+            } 
+           
             
         },200)
+
+    }else if(batteryIsCharging === false && batteryLevel > 90){
+        const batteryContainer = document.getElementById('progres');
+        const div1 = document.createElement('div');
+        div1.classList.add('indicator')
+        const div2 = document.createElement('div');
+        div2.classList.add('indicator')
+        const div3 = document.createElement('div');
+        div3.classList.add('indicator')
+        const div4 = document.createElement('div');
+        div4.classList.add('indicator')
+
+        batteryContainer.appendChild(div1)
+        batteryContainer.appendChild(div2)
+        batteryContainer.appendChild(div3)
+        batteryContainer.appendChild(div4)
        
-       
+    }else if(batteryIsCharging === false && batteryLevel < 50 && batteryLevel > 20 ){
+        const batteryContainer = document.getElementById('progres');
+        const div1 = document.createElement('div');
+        div1.classList.add('indicator')
+        const div2 = document.createElement('div');
+        div2.classList.add('indicator')
+
+        batteryContainer.appendChild(div1)
+        batteryContainer.appendChild(div2)
+
+    }else if(batteryIsCharging === false && batteryLevel < 20){
+        const batteryContainer = document.getElementById('progres');
+        const div1 = document.createElement('div');
+        div1.classList.add('indicator')
+        batteryContainer.appendChild(div1)
     }
+
 }
 
+
+
+
+
 changeDivColor()
+
+
+
 
 
 
