5dea65ea61cb0154e37d3286c36c302df3334d38
ivsp <ivsp93@gmail.com>
2022-02-09 17:58:39 +0100

    APP Cripto currencies

---
+++ students/ivan/web-cripto-monedas/script.js
@@ -0,0 +1,16 @@
+/**
+ * 
+ * Vamos a crear una app de crypto monedas la cual debe lucir como el diseño de la 
+ * imagen, usaremos la api https://www.coingecko.com/es
+ * 
+ * 
+ * 
+ *   1- Para ver el precio de las cripto monedas,  lo primero sera obtener 
+ *      los datos ("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1"),
+ *   2- Muestra en un html la lista de todas las criptomonedas con su precio 
+ *   actual y el  price change.
+ *   3- si el price change es  negativo deberá mostrarse en rojo, si es positivo en verde.
+ *   4- Debera tener un filtro de búsqueda que filtre por nombre de 
+ *   la cripto, basar el diseño en la imagen siguiente.
+ */
+
