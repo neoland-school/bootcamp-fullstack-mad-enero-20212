79091257d051784dc0dcfce566c3b00ef17b6710
macaeco <macarenafernandezayusob@gmail.com>
2022-02-11 09:00:28 +0100

    vaia vaia react bginnsss

---
+++ students/macarena-sotomayor/first-react-app/src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+import HelloWorld from './components/hello-World/helloworld';
+import Cuadro from './components/cuadro/cuadro';
+import ImageCard from './components/card/card'
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <HelloWorld></HelloWorld>
+        <Cuadro></Cuadro>
+        <ImageCard></ImageCard>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
