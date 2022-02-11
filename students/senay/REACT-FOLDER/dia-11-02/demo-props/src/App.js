b888102b49eefd5804f7da558d05df3cbc805945
dedeere400 <dedeere400@gmail.com>
2022-02-11 16:58:27 +0200

    preinstalando windows en mi pc

---
+++ students/senay/REACT-FOLDER/dia-11-02/demo-props/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Greeting from './components/greeting';
+
+function App() {
+  return ( <
+    Greeting > < /Greeting>
+
+  )
+}
+
+export default App;
---
+++ students/senay/REACT-FOLDER/dia-11-02/demo-props/src/components/greeting/index.jsx
@@ -0,0 +1,9 @@
+
+
+
+function Greeting(props) {
+    const saludo = 'Alex';
+  return {<h1>Hola {saludo}</h1>}
+}
+
+export default Greeting;
