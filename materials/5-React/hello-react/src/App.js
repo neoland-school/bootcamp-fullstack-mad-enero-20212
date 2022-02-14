7aaf52da86a4ac1a934ee43cda06c5530f7106ca
Alex González <alejandrogsa@gmail.com>
2022-02-14 11:11:02 +0100

    use state

--- materials/5-React/hello-react/src/App.js
+++ materials/5-React/hello-react/src/App.js
@@ -3,14 +3,16 @@ import './App.css';
 import Greeting from './components/greeting';
 import Anime from './components/anime';
 import { animes, persons } from './data';
+import LifeCycleExample from './components/lifecycle-example';
 
 function App() {
   
   return (
     <React.Fragment>
       {/* {persons.map((p, i) => <Greeting key={i} name={p} extended={p.length < 6}></Greeting>)} */}
-      <h1>Top Anime</h1>
-      <Anime animes={animes}></Anime>
+      {/* <h1>Top Anime</h1>
+      <Anime animes={animes}></Anime> */}
+      <LifeCycleExample></LifeCycleExample>
     </React.Fragment>
   );
 }
