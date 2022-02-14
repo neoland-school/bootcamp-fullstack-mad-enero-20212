2b246be3c7829d35740e9fac1893937d537db0f7
Alba Maria Huertas Dominguez <ahuertasdominguez@gmail.com>
2022-02-14 09:58:21 +0100

    react

--- students/alba-huertas-dominguez/demo-props/src/components/anime/index.jsx
+++ students/alba-huertas-dominguez/demo-props/src/components/anime/index.jsx
@@ -1,13 +1,13 @@
 import react from 'react';
 import './stylesheet.css'
 
-function Anime(prons){
+function Anime(props){
     return (
         <react.Fragment>
     <div className='container'>
-    <img className="imagen" src={prons.img}></img>
-    <h3>{prons.titulo}</h3>
-    <p>{prons.ranking}</p>
+    <img className="imagen" src={props.img}></img>
+    <h3>{props.titulo}</h3>
+    <p>{props.ranking}</p>
     </div>
     </react.Fragment>
     
