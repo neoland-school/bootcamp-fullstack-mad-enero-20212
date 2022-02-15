e5642845709c8db75855f4aec33cd07ccd575b38
Senay <dedeere400@gmail.com>
2022-02-15 13:38:50 +0200

    commit

--- students/senay/REACT-FOLDER/Febrero-React/dia-15-02/src/components/comunication-hijo/index.jsx
+++ students/senay/REACT-FOLDER/Febrero-React/dia-15-02/src/components/comunication-hijo/index.jsx
@@ -1,8 +1,17 @@
-
-
+/**
+ * - Hello Comunicación hijo-padre: Crear dos componentes:
+ * Un componente hijo que tiene un input. Cuando se escriba en el input, se debe enviar su valor al padre.
+ * Un componente padre que tenga el hijo y debajo un p. Este p debe pintar lo que se escriba en el 
+ * input del hijo.
+ */
 function ComunicationHijo() {
+
+    const handleOnChange=(e) => {
+        let inputValue = e.taget.value;
+    };
     return (
-        <input type="text" placeholder="Escriba algo" />
+        <input onChange={handleOnChange} type="text" placeholder="Escriba algo" />
+        
     )
 }
 export default ComunicationHijo;
