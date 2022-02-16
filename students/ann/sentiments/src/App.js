a04f8e3c18736edf9d388f4339cbe0b9fa58acb8
ann <asog.jrti@gmail.com>
2022-02-16 11:11:49 +0100

    styles api response

--- students/ann/sentiments/src/App.js
+++ students/ann/sentiments/src/App.js
@@ -3,10 +3,9 @@ import Sentiment from './components/sentiment';
 
 
 function App() {
-  const api = 'http://text-processing.com/api/sentiment/'
   return (
     <div className="App">
-      <h1>Sentiment Analysis</h1>
+      <h1 className="App__title">Sentiment Analysis</h1>
       <Sentiment></Sentiment>
     </div>
   );
--- students/ann/sentiments/src/components/sentiment/index.jsx
+++ students/ann/sentiments/src/components/sentiment/index.jsx
@@ -1,46 +1,53 @@
-import React, { useState } from "react";
-import './style.css'
+import React, { useState } from 'react';
+import './style.css';
 
-const sentimentAPI = 'http://text-processing.com/api/sentiment/'
-
-// fetch(sentimentAPI, {
-//     method: "POST",
-//     mode: "no-cors",
-//     body: { text: 'great' },
-//     headers: {
-//         'Content-Type': 'application/x-www-form-urlencoded',
-//     }
-// })
-//     .then(response => response.json())
-//     .then(json => console.log(json));
+const sentimentAPI = 'http://text-processing.com/api/sentiment/';
 
 function Sentiment() {
     const [sentiment, update] = useState('');
-    const handleSubmit = e => {
+
+    const handleSubmit = (e) => {
         e.preventDefault();
         const data = new FormData(e.target);
         fetch(sentimentAPI, {
-            method: "POST",
-            mode: "no-cors",
-            body: { text: 'great' },
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-            }
+            method: 'POST',
+            body: data,
         })
-            .then(response => response.json())
-            .then(json => update(json));
-    }
+            .then((response) => response.json())
+            .then((json) => {
+                if (json.label === 'pos') {
+                    update(`"positive" with a probabilty of ${json.probability.pos.toFixed(2)}`);
+                }
+                if (json.label === 'neg') {
+                    update(`"negative" with a probabilty of ${json.probability.neg.toFixed(2)}`);
+                }
+                if (json.label === 'neutral') {
+                    update(`"neutral" with a probabilty of ${json.probability.neutral.toFixed(2)}`);
+                }
+            });
+    };
 
     return (
         <React.Fragment>
-            <form onSubmit={handleSubmit} className="sentiment-form">
-                <label htmlFor="sentiment">Write your sentiment</label>
-                <textarea required name="text" id="sentiment"></textarea>
-                <button type="Submit">Analyze my text</button>
-            </form>
-            <p>{sentiment}</p>
+            <div className="sentiment__form-container">
+                <form onSubmit={handleSubmit} className="sentiment-form">
+                    <label htmlFor="sentiment">Write your sentiment in English</label>
+                    <p className="max__text__sentiment">
+                        (message can't exceeds 80,000 characters)
+                    </p>
+                    <textarea required name="text" id="sentiment"></textarea>
+                    <button id="submit-button" type="Submit">
+                        Analyze my message
+                    </button>
+                </form>
+                <div className="sentiment-response-container">
+                    <p className="sentiment-response">Sentiment analysis response:</p>
+                    <p className="sentiment-response api-response">{sentiment}</p>
+                </div>
+            </div>
+
         </React.Fragment>
-    )
+    );
 }
 
 export default Sentiment;
