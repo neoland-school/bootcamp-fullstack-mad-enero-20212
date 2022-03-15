3978761018e5d6b05c1f0f3148220ae4bbf69050
Alex González <alejandrogsa@gmail.com>
2022-03-15 13:08:49 +0100

    auth backend example

---
+++ materials/6-NodeJS/hello-auth/auth-back/src/users/users.controller.js
@@ -0,0 +1,12 @@
+import { retrieveUserInfoByEmail } from './users.model.js';
+
+export const getUserInfo = async (req, res) => {
+    // llamo al usuario
+    try {
+        const user = await retrieveUserInfoByEmail(req.email);
+        res.json(user); // deveulvo la info del usuario
+    } catch (err) {
+        console.error(err);
+        res.sendStatus(500);
+    }
+}
